5feda71e1deaa248eae7b7c7ad989f25cda1ddaa
Paulo Henrique Martins <paulo.martins@savedo.de>
2017-01-25 13:41:27 +0100

    heroku config

---
+++ config-heroku.js
@@ -0,0 +1,8 @@
+module.exports = {
+  penacova: {
+    contact_url: process.env.CONTACTURL,
+    contact_key: process.env.CONTACTKEY,
+    api_url: process.env.APIURL,
+    api_key: process.env.APIKEY
+  }
+};
